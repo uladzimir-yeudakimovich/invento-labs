a577f73344b643fb2114e31159cc72271dbc46ca
uladzimir-yeudakimovich <uladzimir.yeudakimovich@gmail.com>
2018-09-10 13:51:02 +0300

    added ng-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
+import { NgbCollapseModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './components/products.component';
@@ -32,7 +33,9 @@ const routes: Routes = [
     BrowserModule,
     HttpClientModule,
     FormsModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    NgbModule.forRoot(),
+    NgbCollapseModule
   ],
   providers: [ProductsService],
   bootstrap: [AppComponent]
