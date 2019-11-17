1d70844919403d5c701f471d980ccc837e55eb77
Uladzimir <uladzimir.yeudakimovich@gmail.com>
2019-11-18 00:02:12 +0300

    code improved

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyChRbKLN0Tvh1DttoBL01amEGgm21eS2xw",
+    apiKey: "",
     authDomain: "",
     databaseURL: "",
-    projectId: "1:694245478410:web:86b79fc91e95bf9c554ff3",
+    projectId: "",
     storageBucket: "",
     messagingSenderId: ""
   }
