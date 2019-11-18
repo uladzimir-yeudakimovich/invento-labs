3151e272469ce0c789a364c48e2373cc6b5a2b46
Uladzimir <uladzimir.yeudakimovich@gmail.com>
2019-11-18 10:00:05 +0300

    added 1 test

--- server.js
+++ server.js
@@ -2,6 +2,25 @@ const webdriver = require('selenium-webdriver');
 const browser = new webdriver.Builder().usingServer().withCapabilities({'browserName': 'chrome' }).build();
 browser.get('https://uladzimir-yeudakimovich.github.io/');
 
+function logTitle() {
+  browser.getTitle().then(function(title) {
+    console.log('My name is ' + title);
+  });
+}
+
+function closeBrowser() {
+  browser.quit();
+}
+
+browser.then(logTitle);
+
+browser.findElement(webdriver.By.css('input[formControlName=name]')).sendKeys('Alex');
+browser.findElement(webdriver.By.css('input[formControlName=email]')).sendKeys('alex@gmail.com');
+browser.findElement(webdriver.By.className('form-control_message')).sendKeys('Hi bro!');
+browser.findElement(webdriver.By.className('btn-submit')).click();
+
+
+// THIS CODE FOR MESSAGES
 const messagesRoutes = require('./routes/messages.route');
 
 const express = require('express');
